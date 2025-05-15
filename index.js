6368b340a3b78bc518c9b7c5c90577a175c2b53c
Emmytweg <emmanuelolamide706@gmail.com>
2025-05-15 23:39:50 +0100

    ...

--- index.js
+++ index.js
@@ -7,23 +7,37 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors({ origin: 'https://login-testing-ui.vercel.app' }));
+// const allowedOrigins = ['http://localhost:3000', 'https://login-testing-ui.vercel.app'];
+app.use(cors())
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       return callback(null, true);
+//     } else {
+//       return callback(new Error('Not allowed by CORS'), false);
+//     }
+//   }
+// }));
+
 app.use(express.json());
 
-// MongoDB connection
+// DB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ DB error:', err));
 
-// User model
+// User schema
 const User = mongoose.model('User', new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 }));
-app.get('/', async(req,res)=> {
-  res.json({message: 'Hello World!'})
-})
-// Routes
+
+// Test route
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+// Signup route
 app.post('/signup', async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) return res.status(400).json({ message: 'All fields required' });
